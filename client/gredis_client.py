26805a4eda7e1e0d5932253734abaf09763de5cb
Abhijeet Mohan <void.aby@gmail.com>
2016-04-01 00:02:42 +0530

    Add client and update .gitignore

---
+++ client/gredis_client.py
@@ -0,0 +1,33 @@
+
+
+from grpc.beta import implementations
+import imp
+gredis_pb2 = imp.load_source('gredis_pb2', '../build/generated/gredis_pb2.py')
+
+PORT = 3000
+TIMEOUT_SECONDS = 3
+
+
+def set_value(stub, key, value):
+    request = gredis_pb2.SetValueRequest(key=key, value=value)
+    response = stub.SetValue(request, TIMEOUT_SECONDS)
+    return response.value
+
+def get_value(stub, key):
+    request = gredis_pb2.GetValueRequest(key=key)
+    response = stub.GetValue(request, TIMEOUT_SECONDS)
+    return response.value
+
+def main():
+  channel = implementations.insecure_channel('localhost', PORT)
+  stub = gredis_pb2.beta_create_Gredis_stub(channel)
+
+  # some sample data for testing
+  print 'Running sample data...'
+  set_value(stub, 'foo', 'bar')
+  set_value(stub, 'baz', 'anaconda is here')
+  print get_value(stub, 'foo')
+
+
+if __name__ == '__main__':
+  main()
